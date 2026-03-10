322e5037221def8832ea11b731ee0cb0517f4b0d
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-03-10 09:34:57 +0700

    ubah pjb, ambil price>1000

--- tarf_perkg_nasional_pjb.sql
+++ tarf_perkg_nasional_pjb.sql
@@ -1,37 +1,33 @@
-select location_data_created__custom_field__nopen,
-custom_field__destination_nopen,
-connote__connote_service,
-nilai,
-jumlah,
-rank_service
-FROM
-(select*,
-row_number() OVER(partition by location_data_created__custom_field__nopen,
-custom_field__destination_nopen,
-connote__connote_service
-order by t1.ceiling_berat,t1.connote__created_at DESC) rank_service
-FROM
-(SELECT
-location_data_created__custom_field__nopen,
-custom_field__destination_nopen,
-connote__connote_service,
-date_trunc('month',connote__created_at)connote__created_at,
-(ceiling(nipos.connote__chargeable_weight /10)*10) ceiling_berat,
-round(
-nipos.connote__connote_service_price/
-(ceiling(nipos.connote__chargeable_weight /10)*10),0) nilai,
-COUNT(*)jumlah
+select
+t1.location_data_created__custom_field__nopen,
+t1.custom_field__destination_nopen,
+t1.connote__connote_service,
+t1.hasil,
+t1.jumlah,
+t1.rank_service
+from
+(SELECT 
+    date_trunc('month', nipos.connote__created_at) AS connote_month,
+    nipos.location_data_created__custom_field__nopen,
+    nipos.custom_field__destination_nopen,
+    nipos.connote__connote_service,
+    round(nipos.connote__connote_service_price /
+        (ceiling(connote__chargeable_weight/10)*10),0) AS hasil,
+    COUNT(*) AS jumlah,
+    ROW_NUMBER() OVER (
+        PARTITION BY 
+            nipos.location_data_created__custom_field__nopen,
+            nipos.custom_field__destination_nopen,
+            nipos.connote__connote_service
+        ORDER BY date_trunc('month', nipos.connote__created_at) desc ,COUNT(*) desc,hasil DESC
+    ) AS rank_service
 FROM nipos.nipos
-WHERE location_data_created__custom_field__nopen is not NULL
-and custom_field__destination_nopen is not null
-and nipos.connote__connote_service !='LNINCOMING'
+WHERE nipos.connote__connote_state NOT IN ('CANCEL','PENDING')
+and nipos.location_data_created__custom_field__nopen is not null
+and nipos.custom_field__destination_nopen  is not NULL
+AND nipos.connote__connote_service = 'PJB'
+and nipos.connote__connote_service_price >1000
 and nipos.connote__chargeable_weight >0
-and nipos.connote__connote_amount >0
-and nipos.connote__connote_state not in ('CANCEL','PENDING')
-and nipos.connote__connote_service ='PJB'
-and nipos.connote__created_at >'20240101'
-GROUP BY 1,2,3,4,5,6
---limit 10
+GROUP BY 1,2,3,4,5
 )t1
-)t2
-where rank_service=1
+where t1.rank_service=1
