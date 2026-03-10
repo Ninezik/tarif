06ff29ce498a3b44f97618370ff86cf034b73c9e
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-03-10 10:38:30 +0700

    tarif nasional asli

--- tarif_perkg_nasional.sql
+++ tarif_perkg_nasional.sql
@@ -1,33 +1,36 @@
-SELECT *
-FROM (
-SELECT *,
-ROW_NUMBER() OVER(
-PARTITION BY location_data_created__custom_field__nopen,
+select location_data_created__custom_field__nopen,
+custom_field__destination_nopen,
+connote__connote_service,
+nilai,
+jumlah,
+rank_service
+FROM
+(select*,
+row_number() OVER(partition by location_data_created__custom_field__nopen,
 custom_field__destination_nopen,
 connote__connote_service
-ORDER BY jumlah DESC, nilai DESC
-) AS rank_service
-FROM (
+order by t1.connote__created_at desc,nilai desc) rank_service
+FROM
+(
 SELECT
 location_data_created__custom_field__nopen,
 custom_field__destination_nopen,
 connote__connote_service,
-ROUND(
-connote__connote_service_price / GREATEST(connote__chargeable_weight, 1),
-0
-) AS nilai,
-COUNT(*) AS jumlah
+date_trunc('month',connote__created_at)connote__created_at,
+round(
+nipos.connote__connote_service_price/GREATEST(connote__chargeable_weight, 1)) nilai,
+COUNT(*)jumlah
 FROM nipos.nipos
-WHERE location_data_created__custom_field__nopen is not null
+WHERE location_data_created__custom_field__nopen is not NULL
 and custom_field__destination_nopen is not null
---AND custom_field__jenis_barang = 'Paket'
---          AND connote__connote_service IN ('PJB','PJE')
-and nipos.connote__connote_service !='LN INCOMING'
---AND connote__create_from != 'POSINDOOUTGOING'
-and nipos.connote__chargeable_weight >0
+and nipos.connote__connote_service !='LNINCOMING'
+and nipos.connote__chargeable_weight >=0
 and nipos.connote__connote_amount >0
 and nipos.connote__connote_state not in ('CANCEL','PENDING')
-GROUP BY 1,2,3,4
-) t
-) x
-WHERE rank_service = 1
+and nipos.connote__connote_service !='PJB'
+and nipos.connote__created_at >'20240101'
+GROUP BY 1,2,3,4,5
+)t1
+)t2
+where rank_service=1
+order by 1,2,3
