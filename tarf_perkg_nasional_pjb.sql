732b3b78cbaa6964e19fbb46ff8aca0869e9751a
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-03-10 12:54:26 +0700

    ubah filter ke atas 9.9kg

--- tarf_perkg_nasional_pjb.sql
+++ tarf_perkg_nasional_pjb.sql
@@ -2,32 +2,32 @@ select
 t1.location_data_created__custom_field__nopen,
 t1.custom_field__destination_nopen,
 t1.connote__connote_service,
-t1.hasil,
+t1.nilai,
 t1.jumlah,
 t1.rank_service
 from
-(SELECT 
-    date_trunc('month', nipos.connote__created_at) AS connote_month,
-    nipos.location_data_created__custom_field__nopen,
-    nipos.custom_field__destination_nopen,
-    nipos.connote__connote_service,
-    round(nipos.connote__connote_service_price /
-        (ceiling(connote__chargeable_weight/10)*10),0) AS hasil,
-    COUNT(*) AS jumlah,
-    ROW_NUMBER() OVER (
-        PARTITION BY 
-            nipos.location_data_created__custom_field__nopen,
-            nipos.custom_field__destination_nopen,
-            nipos.connote__connote_service
-        ORDER BY date_trunc('month', nipos.connote__created_at) desc ,COUNT(*) desc,hasil DESC
-    ) AS rank_service
+(SELECT
+date_trunc('month', nipos.connote__created_at) AS connote_month,
+nipos.location_data_created__custom_field__nopen,
+nipos.custom_field__destination_nopen,
+nipos.connote__connote_service,
+round(nipos.connote__connote_service_price /
+(ceiling(connote__chargeable_weight/10)*10),0) AS nilai,
+COUNT(*) AS jumlah,
+ROW_NUMBER() OVER (
+PARTITION BY
+nipos.location_data_created__custom_field__nopen,
+nipos.custom_field__destination_nopen,
+nipos.connote__connote_service
+ORDER BY date_trunc('month', nipos.connote__created_at) desc ,COUNT(*) desc,nilai DESC
+) AS rank_service
 FROM nipos.nipos
 WHERE nipos.connote__connote_state NOT IN ('CANCEL','PENDING')
 and nipos.location_data_created__custom_field__nopen is not null
-and nipos.custom_field__destination_nopen  is not NULL
+and nipos.custom_field__destination_nopen is not NULL
 AND nipos.connote__connote_service = 'PJB'
 and nipos.connote__connote_service_price >1000
-and nipos.connote__chargeable_weight >0
+and nipos.connote__actual_weight  >9.9
 GROUP BY 1,2,3,4,5
 )t1
 where t1.rank_service=1
